e5ce7a95d7f107c7d659052bd96cc686a30fc1bd
JoseMa Esparev <esparev@hotmail.com>
2021-10-10 11:48:26 -0500

    refactor: Change config directory

--- src/server/utils/auth/strategies/basic.js
+++ src/server/utils/auth/strategies/basic.js
@@ -2,7 +2,7 @@ const passport = require("passport");
 const { BasicStrategy } = require("passport-http");
 const boom = require("@hapi/boom");
 const axios = require("axios");
-const { config } = require("../../../config/index");
+const { config } = require("../../../config");
 
 passport.use(
   new BasicStrategy(async function (email, password, cb) {
